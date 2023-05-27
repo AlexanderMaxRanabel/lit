d69737a2b3ca40a17b380467af61d5ce417a6230
AlexanderMaxRanabel <93342430+AlexanderMaxRanabel@users.noreply.github.com>
2023-05-27 10:52:36 +0300

    Getting near to finalize init function

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,53 @@ fn init(name: String) {
             match fs::create_dir(gitpath.clone()) {
                 Ok(()) => {
                     let hookpath = gitpath.as_str().to_owned() + "/hooks";
+                    let infopath = gitpath.as_str().to_owned() + "/info";
+                    let objectspath = gitpath.as_str().to_owned() + "/objects";
+                    let refspath = gitpath.as_str().to_owned() + "/refs";
+
                     match fs::create_dir(hookpath) {
                         Ok(()) => println!("Created Hooks"),
+                        Err(err) => println!("Failed to create directory: {}", err),
+                    }
+
+                    match fs::create_dir(infopath) {
+                        Ok(()) => println!("Created info"),
+                        Err(err) => println!("Failed to create directory: {}", err),
+                    }
+
+                    match fs::create_dir(objectspath.clone()) {
+                        Ok(()) => {
+                            let packpath = objectspath.to_owned() + "/pack";
+                            let infopath2 = objectspath.to_owned() + "/info";
+
+                            match fs::create_dir(packpath) {
+                                Ok(()) => println!("Created pack"),
+                                Err(err) => println!("Failed to create directory: {}", err),
+                            }
+
+                            match fs::create_dir(infopath2) {
+                                Ok(()) => println!("Created info:"),
+                                Err(err) => println!("Failed to create directory: {}", err),
+                            }
+                        },
+                        Err(err) => println!("Failed to create directory: {}", err),
+                    }
+
+                    match fs::create_dir(refspath.clone()) {
+                        Ok(()) => {
+                            let tagspath = refspath.to_owned() + "/tags";
+                            let headspath = refspath.to_owned() + "/heads";
+
+                            match fs::create_dir(tagspath) {
+                                Ok(()) => println!("Created tags"),
+                                Err(err) => println!("Failed to create directory: {}", err),
+                            }
+
+                            match fs::create_dir(headspath) {
+                                Ok(()) => println!("Created heads"),
+                                Err(err) => println!("Failed to create directory: {}", err),
+                            }
+                        },
                         Err(err) => println!("Failed to create directory: {}", err),
                     }
                 },
